97acd9ee56755bfbb6117d9a6fc346411ad0b070
_okyere <121646945+enockaning18@users.noreply.github.com>
2025-10-23 11:19:39 +0000

    login page added

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+require_once('alert.php');
+
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Login</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="" href="" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="bootstrap-config/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="bootstrap-config/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="bootstrap-config/assets/css/demo.css" />
+
+    <link rel="stylesheet" href="bootstrap-config/assets/vendor/css/pages/page-auth.css" />
+    <script src="assets/js/sweetalert.min.js"></script>
+</head>
+
+<body>
+    <!-- Content -->
+
+    <div class="container-xxl ">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <div class="card" style="width: 400px">
+                    <div class="card-body px-5">
+                        <div class="app-brand justify-content-center">
+                            <a href="index.html" class="app-brand-link gap-2">
+                                <img src=" " alt="" class="w-px-100 h-px-100 rounded-circle" />
+                            </a>
+                        </div>
+
+                        <form action=" " method="POST" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Username</label>
+                                <input type="text" class="form-control" id="email" name="userid" placeholder="Enter your email or username" autofocus />
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
+                                    <a href="forgot_password.php">
+                                        <small>Forgot Password?</small>
+                                    </a>
+                                </div>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="userkey" aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="remember-me" />
+                                    <label class="form-check-label" for="remember-me"> Remember Me </label>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" name="auth_login" type="submit">Sign in</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+
+
+</html>
