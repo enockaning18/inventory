9c59e4a195d6e55ba0e6de933598e56860fac47c
_okyere <enockaning18@gmail.com>
2025-10-27 11:28:06 +0000

    database update

--- baseConnect/ipmc_inventory.sql
+++ baseConnect/ipmc_inventory.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 24, 2025 at 06:51 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Oct 27, 2025 at 12:27 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,8 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `brand` (
   `id` int(11) NOT NULL,
-  `brand_name` varchar(100) NOT NULL
+  `brand_name` varchar(100) NOT NULL,
+  `date_added` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`id`, `brand_name`, `date_added`) VALUES
+(3, 'Lenovo', '2025-10-26'),
+(4, 'HP', '2025-10-26');
 
 -- --------------------------------------------------------
 
@@ -49,21 +58,35 @@ CREATE TABLE `computers` (
   `date_added` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `computers`
+--
+
+INSERT INTO `computers` (`id`, `computer_name`, `brand`, `serial_number`, `memory_size`, `hard_drive_size`, `lab`, `date_added`) VALUES
+(11, 'XUAJJA ', 3, 'ASDASADA22', '25GIG', '500GIG', 8, '2025-10-26');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `instructor`
+-- Table structure for table `instructors`
 --
 
-CREATE TABLE `instructor` (
+CREATE TABLE `instructors` (
   `id` int(11) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `phone` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `lab_assigned` int(11) NOT NULL,
-  `Course` varchar(100) NOT NULL
+  `Course` varchar(100) NOT NULL,
+  `date_added` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `instructors`
+--
+
+INSERT INTO `instructors` (`id`, `first_name`, `last_name`, `phone`, `email`, `Course`, `date_added`) VALUES
+(8, 'Sosu', 'Rosemary', '0554944071', 'susorosemaryasd@gmail.com', 'Software Eng', '2025-10-26');
 
 -- --------------------------------------------------------
 
@@ -73,11 +96,12 @@ CREATE TABLE `instructor` (
 
 CREATE TABLE `issues` (
   `id` int(11) NOT NULL,
-  `computer` varchar(100) NOT NULL,
+  `computer` int(11) NOT NULL,
   `issue_type` varchar(100) NOT NULL,
   `lab` int(11) NOT NULL,
   `issue_date` date NOT NULL DEFAULT current_timestamp(),
-  `issue_discription` varchar(250) NOT NULL
+  `issue_description` varchar(250) NOT NULL,
+  `date_added` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -96,6 +120,13 @@ CREATE TABLE `lab` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `lab`
+--
+
+INSERT INTO `lab` (`id`, `lab_name`, `lab_course`, `instructor`, `number_computers`, `date_added`) VALUES
+(8, 'Lab 8', 'Software Eng', 8, '40', '2025-10-26');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -109,19 +140,29 @@ ALTER TABLE `brand`
 -- Indexes for table `computers`
 --
 ALTER TABLE `computers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `brand_id` (`brand`);
+
+--
+-- Indexes for table `instructors`
+--
+ALTER TABLE `instructors`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `issues`
 --
 ALTER TABLE `issues`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `computer_id` (`computer`),
+  ADD KEY `lab_id` (`lab`);
 
 --
 -- Indexes for table `lab`
 --
 ALTER TABLE `lab`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `instructor_id` (`instructor`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -131,25 +172,54 @@ ALTER TABLE `lab`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `computers`
 --
 ALTER TABLE `computers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `instructors`
+--
+ALTER TABLE `instructors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `issues`
 --
 ALTER TABLE `issues`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `lab`
 --
 ALTER TABLE `lab`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `computers`
+--
+ALTER TABLE `computers`
+  ADD CONSTRAINT `brand_id` FOREIGN KEY (`brand`) REFERENCES `brand` (`id`);
+
+--
+-- Constraints for table `issues`
+--
+ALTER TABLE `issues`
+  ADD CONSTRAINT `computer_id` FOREIGN KEY (`computer`) REFERENCES `computers` (`id`),
+  ADD CONSTRAINT `lab_id` FOREIGN KEY (`lab`) REFERENCES `lab` (`id`);
+
+--
+-- Constraints for table `lab`
+--
+ALTER TABLE `lab`
+  ADD CONSTRAINT `instructor_id` FOREIGN KEY (`instructor`) REFERENCES `instructors` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
