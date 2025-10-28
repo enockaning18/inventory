c3a78561b7eb6f0dd3f294da3233b6ad8c6b45f2
Sylvester <smgee43@gmail.com>
2025-10-28 01:29:23 -1200

    analytics upt

--- includes/analytics.php
+++ includes/analytics.php
@@ -1,22 +1,21 @@
-<?php 
-
-?>
+<?php ?>
 
 <div class="cardBox">
+    <!-- first section -->
     <div class="card-toFlex">
         <div class="cardContent">
             <div class="numbers">0</div>
-            <div class="cardName">Label</div>
+            <div class="cardName">Examinations</div>
         </div>
         <div class="iconBx">
-            <ion-icon name="eye-outline"></ion-icon>
+            <ion-icon name="book-outline"></ion-icon>
         </div>
     </div>
 
     <div class="card-toFlex">
         <div class="cardContent">
             <div class="numbers">0</div>
-            <div class="cardName">Label</div>
+            <div class="cardName">CancelledExams</div>
         </div>
         <div class="iconBx">
             <ion-icon name="cart-outline"></ion-icon>
@@ -26,7 +25,7 @@
     <div class="card-toFlex">
         <div class="cardContent">
             <div class="numbers">0</div>
-            <div class="cardName">Label</div>
+            <div class="cardName">Courses</div>
         </div>
         <div class="iconBx">
             <ion-icon name="chatbubbles-outline"></ion-icon>
@@ -37,7 +36,49 @@
         <div class="cardContent">
             <div class="numbers">0</div>
             
-            <div class="cardName">Label</div>
+            <div class="cardName">Modules</div>
+        </div>
+        <div class="iconBx">
+            <ion-icon name="cash-outline"></ion-icon>
+        </div>
+    </div>
+
+    <!-- second section -->
+    <div class="card-toFlex">
+        <div class="cardContent">
+            <div class="numbers">0</div>
+            <div class="cardName">Instructors</div>
+        </div>
+        <div class="iconBx">
+            <ion-icon name="eye-outline"></ion-icon>
+        </div>
+    </div>
+
+    <div class="card-toFlex">
+        <div class="cardContent">
+            <div class="numbers">0</div>
+            <div class="cardName">Computers</div>
+        </div>
+        <div class="iconBx">
+            <ion-icon name="cart-outline"></ion-icon>
+        </div>
+    </div>
+
+    <div class="card-toFlex">
+        <div class="cardContent">
+            <div class="numbers">0</div>
+            <div class="cardName">Replacements</div>
+        </div>
+        <div class="iconBx">
+            <ion-icon name="chatbubbles-outline"></ion-icon>
+        </div>
+    </div>
+
+    <div class="card-toFlex">
+        <div class="cardContent">
+            <div class="numbers">0</div>
+            
+            <div class="cardName">Issues</div>
         </div>
         <div class="iconBx">
             <ion-icon name="cash-outline"></ion-icon>
