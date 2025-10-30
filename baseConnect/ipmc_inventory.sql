0d714b6debe1acbaeae96dc8259f9ce18de53629
Sylvester <smgee43@gmail.com>
2025-10-30 01:43:08 -1200

    Update ipmc_inventory.sql

--- baseConnect/ipmc_inventory.sql
+++ baseConnect/ipmc_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2025 at 09:36 PM
+-- Generation Time: Oct 30, 2025 at 02:42 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -39,7 +39,9 @@ CREATE TABLE `brand` (
 
 INSERT INTO `brand` (`id`, `brand_name`, `date_added`) VALUES
 (1, 'Lenovo ', '2025-10-27'),
-(2, 'HP', '2025-10-28');
+(2, 'HP', '2025-10-28'),
+(3, 'Dell', '2025-10-29'),
+(6, 'Toshiba', '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -64,7 +66,8 @@ CREATE TABLE `computers` (
 
 INSERT INTO `computers` (`id`, `computer_name`, `brand`, `serial_number`, `memory_size`, `hard_drive_size`, `lab`, `date_added`) VALUES
 (2, 'DESKTOP-N9R4ILO', 1, '26100.6899', '16', '500', 1, '2025-10-27'),
-(3, 'i5 Computer', 2, '123456789', '16', '500', 1, '2025-10-29');
+(3, 'i5 Computer', 2, '73828972012', '16', '500', 1, '2025-10-29'),
+(5, 'i3 Computer', 3, '1923989320', '32', '256', 3, '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -84,8 +87,12 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`id`, `course_name`, `createdby`, `datecreated`) VALUES
-(4, 'Database', 0, '2025-10-28'),
-(6, 'Software Engineering', 0, '2025-10-28');
+(4, 'Database', 4, '2025-10-28'),
+(6, 'Software Engineering', 4, '2025-10-28'),
+(8, 'Graphics & Web', 4, '2025-10-29'),
+(11, 'IT @ Workplace', 3, '2025-10-29'),
+(12, 'Graphics & Web Design', 3, '2025-10-29'),
+(14, 'Software', 4, '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -113,7 +120,10 @@ CREATE TABLE `examination` (
 --
 
 INSERT INTO `examination` (`id`, `examination_date`, `batch_time`, `session`, `course_id`, `date_booked`, `start_time`, `module_id`, `instructor_id`, `batch_semester`, `status`, `date_added`) VALUES
-(4, '2025-10-29', '7am - 9am', 'Weekday', 4, '2025-10-29', '03:00', 1, 0, 'Sem-1', 'pending', '2025-10-29');
+(4, '2025-10-29', '7am - 9am', 'Weekday', 4, '2025-10-29', '15:00', 1, 7, 'Sem-1', 'approve', '2025-10-29'),
+(5, '2025-10-29', '9am - 11am', 'Weekday', 4, '2025-10-29', '09:00', 2, 8, 'Sem-1', 'cancelled', '2025-10-29'),
+(6, '2025-10-31', '1pm - 3pm', 'Weekday', 8, '2025-10-28', '13:00', 5, 9, 'Sem-1', 'pending', '2025-10-29'),
+(7, '2025-10-31', '11am - 1pm', 'Weekday', 6, '2025-10-30', '11:00', 2, 7, 'Sem-1', 'approve', '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -126,7 +136,7 @@ CREATE TABLE `instructors` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `lab_id` int(11) NOT NULL,
-  `phone` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `course_id` int(11) NOT NULL,
   `date_added` date NOT NULL DEFAULT current_timestamp()
@@ -137,7 +147,9 @@ CREATE TABLE `instructors` (
 --
 
 INSERT INTO `instructors` (`id`, `first_name`, `last_name`, `lab_id`, `phone`, `email`, `course_id`, `date_added`) VALUES
-(5, 'Okyere', 'Enock', 1, '05560616477', 'okyere@gmail.com', 6, '2025-10-28');
+(7, 'Unicom', 'Center', 1, '0240526430', 'mckenzieraney43@gmail.com', 4, '2025-10-29'),
+(8, 'Daniel', 'Darko', 4, '0260613128', 'info@luxuryshippinghome.com', 8, '2025-10-29'),
+(9, 'Agyei', 'Emmanuel', 3, '0260614128', 'smgee43@gmail.com', 6, '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -160,8 +172,9 @@ CREATE TABLE `issues` (
 --
 
 INSERT INTO `issues` (`id`, `computer`, `issue_type`, `lab`, `issue_date`, `issue_description`, `date_added`) VALUES
-(4, 2, 'Software', 1, '2025-10-01', 'NON CMOS', '2025-10-27'),
-(6, 3, 'Hardware', 1, '2025-10-29', 'Computer can\\\'t run applications', '2025-10-29');
+(4, 2, 'Software', 1, '2025-10-01', 'Auto Restart', '2025-10-27'),
+(6, 3, 'Hardware', 1, '2025-10-29', 'Computer can not run applications', '2025-10-29'),
+(8, 5, 'Hardware', 3, '2025-10-29', 'Memory fault detected needs replacement', '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -182,7 +195,9 @@ CREATE TABLE `lab` (
 --
 
 INSERT INTO `lab` (`id`, `lab_name`, `course_id`, `number_computers`, `date_added`) VALUES
-(1, 'Lab2', 4, '10', '2025-10-27');
+(1, 'Lab2', 4, '10', '2025-10-27'),
+(3, 'Lab4', 6, '30', '2025-10-29'),
+(4, 'Lab3', 8, '25', '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -193,6 +208,8 @@ INSERT INTO `lab` (`id`, `lab_name`, `course_id`, `number_computers`, `date_adde
 CREATE TABLE `module` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `semester` varchar(20) NOT NULL,
+  `course_id` int(11) NOT NULL,
   `date_created` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -200,10 +217,13 @@ CREATE TABLE `module` (
 -- Dumping data for table `module`
 --
 
-INSERT INTO `module` (`id`, `name`, `date_created`) VALUES
-(1, 'C++', '2025-10-29'),
-(2, 'Programming Methods', '2025-10-29'),
-(3, 'CoreJava', '2025-10-29');
+INSERT INTO `module` (`id`, `name`, `semester`, `course_id`, `date_created`) VALUES
+(1, 'C#.NET', 'Sem-2', 6, '2025-10-29'),
+(2, 'Programming Methods', 'Sem-1', 6, '2025-10-29'),
+(3, 'Core Java', 'Sem-1', 6, '2025-10-29'),
+(5, 'Adobe Premier', 'Sem-2', 12, '2025-10-29'),
+(6, 'PowerBI', 'Sem-1', 4, '2025-10-29'),
+(8, 'System Analysis & Design', 'Sem-2', 8, '2025-10-29');
 
 -- --------------------------------------------------------
 
@@ -214,11 +234,18 @@ INSERT INTO `module` (`id`, `name`, `date_created`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `usert_ype` varchar(30) NOT NULL COMMENT '1-admin\r\n2-instructor\r\n3-student',
+  `user_type` varchar(30) NOT NULL COMMENT '1-admin\r\n2-instructor\r\n3-student',
   `instructor_id` int(11) NOT NULL,
-  `user_key` varchar(50) NOT NULL,
+  `user_key` varchar(255) NOT NULL,
   `date_created` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `user_type`, `instructor_id`, `user_key`, `date_created`) VALUES
+(7, 'admin@email.com', 'admin', 7, '$2y$10$QD2LLGF9SY3n.TpWzTuAmexHUMLmNg/7CMYHKJx3psFC8/y3S/P0q', '2025-10-30');
 
 --
 -- Indexes for dumped tables
@@ -242,7 +269,9 @@ ALTER TABLE `computers`
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `course_name` (`course_name`),
+  ADD KEY `createdby_fk` (`createdby`);
 
 --
 -- Indexes for table `examination`
@@ -250,13 +279,16 @@ ALTER TABLE `course`
 ALTER TABLE `examination`
   ADD PRIMARY KEY (`id`),
   ADD KEY `courseid_fk` (`course_id`),
-  ADD KEY `moduleid_fk` (`module_id`);
+  ADD KEY `moduleid_fk` (`module_id`),
+  ADD KEY `instructorid_fk` (`instructor_id`);
 
 --
 -- Indexes for table `instructors`
 --
 ALTER TABLE `instructors`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone` (`phone`),
+  ADD UNIQUE KEY `email` (`email`),
   ADD KEY `fk_lab_id` (`lab_id`),
   ADD KEY `fk_course_id` (`course_id`);
 
@@ -280,14 +312,16 @@ ALTER TABLE `lab`
 --
 ALTER TABLE `module`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `course_fk_id` (`course_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `instructor_id_fk` (`instructor_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -297,55 +331,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `computers`
 --
 ALTER TABLE `computers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `examination`
 --
 ALTER TABLE `examination`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `instructors`
 --
 ALTER TABLE `instructors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `issues`
 --
 ALTER TABLE `issues`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `lab`
 --
 ALTER TABLE `lab`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `module`
 --
 ALTER TABLE `module`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -359,10 +393,17 @@ ALTER TABLE `computers`
   ADD CONSTRAINT `lab_id` FOREIGN KEY (`lab`) REFERENCES `lab` (`id`) ON UPDATE CASCADE;
 
 --
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `createdby_fk` FOREIGN KEY (`createdby`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+
+--
 -- Constraints for table `examination`
 --
 ALTER TABLE `examination`
   ADD CONSTRAINT `courseid_fk` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `instructorid_fk` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `moduleid_fk` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`) ON UPDATE CASCADE;
 
 --
@@ -384,6 +425,18 @@ ALTER TABLE `issues`
 --
 ALTER TABLE `lab`
   ADD CONSTRAINT `fk_courseid` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `module`
+--
+ALTER TABLE `module`
+  ADD CONSTRAINT `course_fk_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `instructor_id_fk` FOREIGN KEY (`instructor_id`) REFERENCES `instructors` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
